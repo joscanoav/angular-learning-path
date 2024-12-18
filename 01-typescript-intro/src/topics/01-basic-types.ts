3f5ce846bf928e4798a8400fa0b214d23aa24bf5
Jorge Oscanoa <jorge.oscanoa@ext.mtp.es>
2024-12-18 10:58:00 +0100

    Add basic types and general functionalities

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,12 @@
+const name: string = 'Strider';
+let hpPoints: number | string = 95;
+hpPoints = 'FULL';
+const isAlive: boolean = true;
+
+console.log({
+
+    name, hpPoints, isAlive
+
+});
+
+export{};
