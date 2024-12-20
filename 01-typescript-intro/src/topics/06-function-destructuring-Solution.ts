768f94ee782b9becd77668760893bdcff2096f42
joscanoav <joscanoav@gmail.com>
2024-12-20 11:56:19 +0100

    Add examples of imports and exports in TypeScript

--- 01-typescript-intro/src/topics/06-function-destructuring-Solution.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring-Solution.ts
@@ -1,4 +1,4 @@
-interface Product {
+export interface Product {
     description: string;
     price: number;
 };
@@ -23,7 +23,7 @@ interface TaxCalculationOptions{
 // creamos la funcion
 //function taxCalculation( options: TaxCalculationOptions) : [number,number]{
 //function taxCalculation( {tax, products}: TaxCalculationOptions) : [number,number]{
-    function taxCalculation( options: TaxCalculationOptions) : [number,number]{
+  export function taxCalculation( options: TaxCalculationOptions) : [number,number]{
     const {tax, products} = options;
     let total = 0;
     products.forEach( ({price}) => {
@@ -47,4 +47,4 @@ console.log('Tax', TaxTotal)
 // creamos constantes / es una variable
 // En TypeScript, aunque las constantes, objetos y funciones pueden parecer conceptos similares porque todos pueden almacenarse en variables
 
-export {};
+
