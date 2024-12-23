8ae353a53a2fb221c8b962de1fa6ff176f4a87ff
joscanoav <joscanoav@gmail.com>
2024-12-23 11:11:03 +0100

    Add examples demonstrating composition over inheritance in TypeScript

--- 01-typescript-intro/src/topics/08-Classes.ts
+++ 01-typescript-intro/src/topics/08-Classes.ts
@@ -1,23 +1,41 @@
+//import { Person } from './08-Classes';
 export class Person{
     //public name: string;
     //private address: string;
 
     constructor(
-        public name:string,
+        public firstName:string,
+        public lastName:string,
         private address: string = 'No Address'
     ){}
 }
 
-export class Hero extends Person{
+// export class Hero extends Person{
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName:string,
+//     ){
+//         super(realName, 'New York');
+//     }
+
+// }
+
+export class Hero {
+
+    //public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
         public realName:string,
+        public person : Person,
     ){
-        super(realName, 'New York');
+        //this.person = new Person(realName);
     }
 
 }
 
-const iroman = new Hero('Iroman',45,'Tony');
+const tony = new Person('TonyStark','Stark','New York');
+const iroman = new Hero('Iroman',45,'Tony',tony);
 console.log(iroman)
