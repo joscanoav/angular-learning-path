0d8c379812d9620483890ae9846418963e45a107
joscanoav <joscanoav@gmail.com>
2025-01-07 15:18:54 +0100

    create Counter Component with basic features in Angular

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
