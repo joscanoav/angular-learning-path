15f7d29d014ca474586f1c1c5a8766576f689cca
joscanoav <joscanoav@gmail.com>
2025-01-07 14:55:44 +0100

    Add Counter component with basic functionality in Angular

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -5,7 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   standalone: false,
 })
+
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number=10;
+
+  increaseBy(value: number):void{
+    this.counter +=value;
+  }
+
+  resetCounter(){
+    this.counter = 10;
+  }
+
+
 }
