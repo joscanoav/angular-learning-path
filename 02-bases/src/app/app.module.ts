f09313663ad179b451e58d307493e1f765d40135
joscanoav <joscanoav@gmail.com>
2025-01-08 14:31:40 +0100

    Completed task on Angular modules implementation

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/hero/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
+    HeroesModule,
 
   ],
   providers: [],
