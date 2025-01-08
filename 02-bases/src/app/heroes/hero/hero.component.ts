3c0f1fb903c5d3a6a40b4b97252330df6a76da7f
joscanoav <joscanoav@gmail.com>
2025-01-08 10:03:57 +0100

    Implemented one-way data bliding in Hero component

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -12,4 +12,13 @@ export class HeroComponent {
   public name: string = 'iroman';
   public age: number = 45;
 
+  get capitalizedName():string{
+    return this.name.toLocaleUpperCase();
+  }
+
+  getHeroDescription():string{
+
+    return `${this.name} - ${this.age}`;
+  }
+
 }
