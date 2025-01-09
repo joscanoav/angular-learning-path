d7f38315a1f96d8266c3f2375cd6d7330482f1c4
joscanoav <joscanoav@gmail.com>
2025-01-09 12:22:38 +0100

    Designed small reusable components for the UI

---
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,13 @@
+/* ng g component dbz/components/list */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-list',
+  standalone: false,
+
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
