7159a28ba4b38363117b02ea5a77b0d915279f12
joscanoav <joscanoav@gmail.com>
2025-01-09 11:47:37 +0100

    Designed layout for the working screen

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/hero/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
 
 
 @NgModule({
@@ -16,6 +18,7 @@ import { HeroesModule } from './heroes/hero/heroes.module';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
 
   ],
   providers: [],
