dbaed439f72bb7b236ac5790f4f69523e8565133
joscanoav <joscanoav@gmail.com>
2025-01-13 14:28:03 +0100

    Add private service

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -11,8 +11,19 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent   {
 
-  constructor(private dbzService: DbzService ){
+  constructor(private dbzService: DbzService ){  }
 
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+
+  }
+
+  onDeleteCharacter(id: string): void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter( character: Character):void{
+    this.dbzService.addCharacter(character);
   }
 
 }
