008f17c22b09a63e240f96533b013da6e13655f7
joscanoav <joscanoav@gmail.com>
2025-01-20 13:17:46 +0100

    Displayed country data on the UI

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,10 +10,15 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
-  searchByCapital ( term: string) : void {
-    console.log('Desde ByCapitalPage')
-    console.log({term});
+  public countries: Country[]=[];
 
+  constructor ( private CountriesService: CountriesService ){}
+
+  searchByCapital ( term: string) : void {
+    this.CountriesService.searchCapital( term )
+    .subscribe( countries => {
+      this.countries= countries;
+    })
   }
 
 }
