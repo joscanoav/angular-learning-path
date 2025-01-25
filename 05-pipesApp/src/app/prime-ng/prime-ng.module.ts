9797b7e21e21a2600be69abffb68a3bd42f6382e
joscanoav <joscanoav@gmail.com>
2025-01-25 21:12:47 +0100

    Created basic structure for the top menu using PrimeNG components

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+
+import { MenubarModule } from 'primeng/menubar';
 import { MenuModule } from 'primeng/menu';
+
 
 
 @NgModule({
   exports: [
+    MenubarModule,
     MenuModule,
   ]
 })
