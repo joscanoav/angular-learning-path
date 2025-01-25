4348a0eff95d9fc5de3477d6ab25389f1e2e3b9a
joscanoav <joscanoav@gmail.com>
2025-01-25 20:27:22 +0100

    Added PrimeNG components for the top menu

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,9 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
-  ],
+    SharedModule,
+    BrowserAnimationsModule,
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+      this.menuItems = [
+          { label: 'New', icon: 'pi pi-plus' },
+          { label: 'Search', icon: 'pi pi-search' }
+      ];
+  }
+
+}
