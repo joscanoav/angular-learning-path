0fe70da7763b9953246f235ed1af95346fe79bea
joscanoav <joscanoav@gmail.com>
2025-01-26 01:27:44 +0100

    Added Decimal, Currency y Percent

--- 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./numbers-page.component.css']
 })
 export class NumbersPageComponent {
+  public totalSells: number = 2567789.5564;
+  public percent: number = 0.4856;
 
 }
