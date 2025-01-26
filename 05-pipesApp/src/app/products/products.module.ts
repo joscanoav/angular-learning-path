073205ed95e177494a39f7a29849ac4da4333ac3
joscanoav <joscanoav@gmail.com>
2025-01-26 20:40:48 +0100

    Implemented  ToggleCasePipe for text transformation

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -8,13 +8,17 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 
+import { ToggleCasePipe } from './pipes/toggle-case.pipes';
+
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
     UncommonPageComponent,
-    OrderComponent
+    OrderComponent,
+    //Pipes
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
