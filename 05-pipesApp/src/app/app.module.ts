9b8f038360918c576ce272b6b6a3a18696e432fc
joscanoav <joscanoav@gmail.com>
2025-01-26 01:10:41 +0100

    Implemented global and manual language switching

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
+
+//Configuracion del locale de la app
+import localeEsPE from '@angular/common/locales/es-PE'
+import localeFrCa from '@angular/common/locales/fr-CA'
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData ( localeEsPE );
+registerLocaleData ( localeFrCa );
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +29,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     BrowserAnimationsModule,
 ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-PE'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
