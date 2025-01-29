986f4b7d9dc0bf1da6889abad7796941d675a81f
joscanoav <joscanoav@gmail.com>
2025-01-29 21:02:42 +0100

    Add registration form

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Validator } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+
+   public myForm: FormGroup = this.fb.group({
+    name:['',[ Validators.required ]],
+    email:['',[ Validators.required ]],
+    username:['',[ Validators.required ]],
+    password:['',[ Validators.required, Validators.minLength(6)]],
+    password2:['',[ Validators.required ]],
+
+   });
+
+   constructor (private fb: FormBuilder){}
+
+   isValidField (field: string){
+    //TODO: obtener validacion desde un servicio
+   }
+
+   onSubmit(){
+    this.myForm.markAllAsTouched();
+   }
+
 
 }
