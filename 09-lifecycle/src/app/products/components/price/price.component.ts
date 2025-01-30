d7930509aaeb7eb2713271c197a758309fead36a
joscanoav <joscanoav@gmail.com>
2025-01-30 17:23:58 +0100

    Added ngDestroy

--- 09-lifecycle/src/app/products/components/price/price.component.ts
+++ 09-lifecycle/src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -11,8 +12,11 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription;
+
   ngOnInit(): void {
-    console.log('Componente hijo: ngOnInit')
+    console.log('Componente hijo: ngOnInit');
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`));
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Componente hijo: ngOnchanges')
@@ -20,6 +24,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   }
   ngOnDestroy(): void {
     console.log('Componente hijo: ngOnDestroy')
+    this.interval$?.unsubscribe();
   }
 
 
