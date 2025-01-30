41281343221f0661a48846eb8665ec700b07ecf2
joscanoav <joscanoav@gmail.com>
2025-01-30 10:49:35 +0100

    Add region selector

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,10 +16,17 @@ export class SelectorPageComponent {
     country: ['', Validators.required],
     borders: ['', Validators.required],
 
-  })
+  });
 
   constructor(
     private fb: FormBuilder,
+    private countriesService : CountriesService,
   ){}
+
+  get regions(): Region[]{
+    return this.countriesService.regions;
+  }
+
+
 
 }
