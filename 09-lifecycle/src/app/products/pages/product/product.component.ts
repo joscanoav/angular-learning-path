97570b6bb59dfcd6b3eda9363d8eb8dc3bacb9c3
joscanoav <joscanoav@gmail.com>
2025-01-30 17:11:26 +0100

    Implemented ngOnChanges

--- 09-lifecycle/src/app/products/pages/product/product.component.ts
+++ 09-lifecycle/src/app/products/pages/product/product.component.ts
@@ -8,6 +8,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   public isProductVisible: boolean =false;
+  public currentPrice: number = 10;
 
   constructor(){
     console.log('Constructor')
@@ -37,6 +38,10 @@ export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterConten
   }
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+  }
+
+  increasePrice(){
+    this.currentPrice++
   }
 
 }
