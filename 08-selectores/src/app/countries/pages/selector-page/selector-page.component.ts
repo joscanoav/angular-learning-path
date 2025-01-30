538f257af8b829737f8115b974c26dc67aa13130
joscanoav <joscanoav@gmail.com>
2025-01-30 11:31:11 +0100

    Set up API call for countries by selected region

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
 import { __values } from 'tslib';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -34,8 +35,11 @@ export class SelectorPageComponent implements OnInit {
 
   onRegionChanged():void {
     this.myForm.get('region')!.valueChanges
-    .subscribe( region =>{
-      console.log({ region })
+    .pipe(
+      switchMap (region => this.countriesService.getCountriesByRegion(region)),
+    )
+    .subscribe( countries =>{
+      console.log({ countries })
     });
   }
 
